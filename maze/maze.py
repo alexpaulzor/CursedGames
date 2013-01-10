85c8a9bf996481e6b2c7556b62936e8862787a09
Alex Paul <apaul@liveramp.com>
2013-01-10 15:25:18 -0800

    progress?

--- maze/maze.py
+++ maze/maze.py
@@ -10,7 +10,7 @@ class Maze:
         self.width = width
         self.height = height
         self.newboard(width, height)
-        curses.wrapper(self.newgame)
+        #curses.wrapper(self.newgame)
 
 
     def newgame(self, stdscr):
@@ -34,6 +34,31 @@ class Maze:
                     self.stdscr.addstr(y, x * 2 + 1, "|")
                 elif square.down_wall:
                     self.stdscr.addstr(y, x * 1, "__")
+    
+    def outputboard(self):
+        print
+        print "Current: %s" % self.current_square
+        line = ""
+        for x in range(len(self.board[0])):
+            line += "__"
+        print line
+        for y in self.board:
+            line = ""
+            if self.board[y][0] == self.enter_square:
+                line += "S"
+            else:
+                line += "|"
+            for x in self.board[y]:
+                square = self.board[y][x]
+                if square.down_wall and square.right_wall:
+                    line += "_|"
+                elif square.right_wall:
+                    line += " |"
+                elif square.down_wall:
+                    line += "__"
+            print line
+        print
+
 
 
 
@@ -51,50 +76,44 @@ class Maze:
         self.enter_square = self.board[0][0]
         if rand.randint(0, 1) == 0:
             self.exit_square = self.board[height - 1][rand.randint(0, width - 1)]
+            self.exit_square.down_wall = False
         else:
             self.exit_square = self.board[rand.randint(0, height - 1)][width - 1]
-        self.exit_square.visited = True
+            self.exit_square.right_wall = False
+        #self.exit_square.visited = False
 
-        tovisit = [self.exit_square]
 
-        while len(tovisit) > 0:
-            current = tovisit.pop()
-            print "CURRENT: %s" % current
-            tovisit += self.explore_neighbors(current)
+        self.explore_neighbors(self.enter_square)
 
     def explore_neighbors(self, square):
+        """
+            Explore unvisited neighbors in random order
+            
+            
+        """
+        square.visited = True
+        self.current_square = square
         nbrs = []
         if square.y > 0:
-            s = self.board[square.y - 1][square.x]
-            if not s.visited:
-                s.visited = True
-                s.down_wall = False
-                nbrs.append(s)
-                print s
+            nbrs.append(self.board[square.y - 1][square.x])
         if square.x > 0:
-            s = self.board[square.y][square.x - 1]
-            if not s.visited:
-                s.visited = True
-                s.right_wall = False
-                nbrs.append(s)
-                print s
+            nbrs.append(self.board[square.y][square.x - 1])
         if square.y < self.height - 1:
-            s = self.board[square.y + 1][square.x]
-            if not s.visited:
-                s.visited = True
-                square.down_wall = False
-                nbrs.append(s)
-                print s
+            nbrs.append(self.board[square.y + 1][square.x])
         if square.x < self.width - 1:
-            s = self.board[square.y][square.x + 1]
-            if not s.visited:
-                s.visited = True
-                square.right_wall = False
-                nbrs.append(s)
-                print s
+            nbrs.append(self.board[square.y][square.x + 1])
+        
         rand = random.Random()
         rand.shuffle(nbrs)
-        return nbrs
+        
+        self.outputboard()
+
+        for nbr in nbrs:
+            if not nbr.visited:
+                #nbr.visited = True
+                square.destroy_wall(nbr)
+                self.explore_neighbors(nbr)
+
 
 
 
@@ -114,7 +133,21 @@ class Square:
         self.down_wall = True
         self.visible = False
         self.visited = False
-    
+
+    def destroy_wall(self, other):
+        print "Destroying wall %s, %s" % (self, other)
+        if other.x == self.x:
+            if other.y == self.y - 1:
+                other.down_wall = False
+            elif other.y == self.y + 1:
+                self.down_wall = False
+        elif other.y == self.y:
+            if other.x == self.x - 1:
+                other.right_wall = False
+            elif other.x == self.x + 1:
+                self.right_wall = False
+        print "Destroyed wall %s, %s" % (self, other)
+
     def __str__(self):
         if self.down_wall:
             d = "_"
@@ -125,7 +158,12 @@ class Square:
         else:
             r = " "
 
-        return "(%d,%d)%s%s" % (self.x, self.y, d, r)
+        if self.visited:
+            v = "v"
+        else:
+            v = " "
+
+        return "[%s](%d,%d)%s%s" % (v, self.x, self.y, d, r)
 
 
 if __name__ == "__main__":
